d0a600044b4227f6d2b36f588b508c813414fecf
andrewearls <andrew.e.earls@gmail.com>
2022-04-10 18:01:29 -0700

    removed the secret key
    Google maps integration

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -541,13 +541,13 @@
                 <p>(971) 915-3748</p>
               </div>
 
-              <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12097.433213460943!2d-74.0062269!3d40.7101282!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xb89d1fe6bc499443!2sDowntown+Conference+Center!5e0!3m2!1smk!2sbg!4v1539943755621" frameborder="0" style="border:0; width: 100%; height: 290px;" allowfullscreen></iframe>
+              <iframe src="https://www.google.com/maps/embed/v1/place?key={{ env('GOOGLE_MAPS_KEY') }}=Space+Needle,Seattle+WA" frameborder="0" style="border:0; width: 100%; height: 290px;" allowfullscreen></iframe>
             </div>
 
           </div>
 
           <div class="col-lg-7 mt-5 mt-lg-0 d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="200">
-            <form action="{{ route('message.new') }}" method="post" role="form" class="php-email-form">
+            <form action="{{ route('messages.store') }}" method="post" role="form" class="php-email-form">
               @csrf
               <div class="form-row">
                 <div class="form-group col-md-6">
